9654f5eeb4fe7d37f651b3c453e3b8d1a4aa29d3
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-24 21:17:53 +0530

    Time: 16 ms (31.04%) | Memory: 9.6 MB (81.54%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int c ,mx= 1;
+    int mx= 1;
         for(string &i : sentences) {
-            c=1 + count(i.begin() ,i.end() , ' ');
+            int c=1 + count(i.begin() ,i.end() , ' ');
             mx=max(mx ,c );
         }
         return mx;
