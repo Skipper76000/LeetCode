052fc5dc99569c2d1298aa45635c009c5c3a6c9e
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-24 21:16:23 +0530

    Time: 17 ms (21.16%) | Memory: 9.7 MB (59.01%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -3,10 +3,7 @@ public:
     int mostWordsFound(vector<string>& sentences) {
         int c ,mx= 1;
         for(string &i : sentences) {
-            c=1;
-            for(char &j: i) {
-                if(j == ' ') c++;
-            }
+            c=1 + count(i.begin() ,i.end() , ' ');
             mx=max(mx ,c );
         }
         return mx;
