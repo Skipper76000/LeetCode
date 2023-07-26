37630cf7d982fd611dce77b25875b4e9b5c1603b
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-26 18:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (39.82%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,22 +1,11 @@
 class Solution {
-
-private:
-    int solve(int n , int s , int e ) {
-        if(s > e) return s-1;
-        if(n == 1 || n==0) return n;
-
-        long long mid = s + (e -s)/2;
-
-        if(mid*mid == n) return mid;
-
-        if( mid*mid > n) return solve(n, s, mid-1);
-        else return solve(n, mid+1 , e);
-
-    }
-
 public:
     int mySqrt(int x) {
-        int a= solve(x , 0 , x);
-        return a;
+        if (x == 0) return 0;
+        long long int sqrtX = x; // Convert to long long to prevent overflow
+        while (sqrtX * sqrtX > x) {
+            sqrtX = (sqrtX + x / sqrtX) / 2;
+        }
+        return static_cast<int>(sqrtX);
     }
 };
