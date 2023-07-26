244d2e1d02bebe33508cf5808657e4085483cd2e
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-26 20:15:31 +0530

    Time: 418 ms (5.03%) | Memory: 82.4 MB (5.49%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,22 +1,31 @@
 class Solution {
+private:
+
+    bool check(vector<int>& v, int k , int s ,int e) {
+        if(s > e) return false;
+
+        int m = s+ (e -s)/2;
+
+        if(v[m] == k) return true;
+
+        if(v[m] > k) return check(v , k, s ,m-1);
+        else return check(v, k ,m+1 ,e);
+    }
 
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> mp;
         vector<int> v;
-        for(auto &i: nums1) {
-            mp[i]++;
-        }
-        int mi =INT_MAX;
+        set<int> s;
 
-        for(auto &i :nums2) {
-            if(mp.find(i) != mp.end()) {
-                mi=min(mi, i);
-                mp[i]--;
-                if(mp[i] == 0) {
-                    mp.erase(i);
-                }
-            }
+        for(auto &i: nums1) {
+            s.insert(i);
+        }
+        for(auto &i : nums2) {
+            s.insert(i);
+        }
+        int mi=INT_MAX;
+        for(auto &i :s){
+            if(check(nums2 , i , 0 , nums2.size()-1) && check(nums1 , i , 0 , nums1.size()-1)){ mi =min(mi,i);}
         }
         return mi==INT_MAX?-1:mi;
     }
