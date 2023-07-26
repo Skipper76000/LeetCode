435e1981568e86104a8493d1fc07d6bc49d3f53c
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-26 18:50:42 +0530

    Time: 35 ms (10.42%) | Memory: 18 MB (12.58%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,30 +1,19 @@
 class Solution {
-// private:
+private:
 
-//     int solve(vector<int>& v, int s, int e) {
-//         if (s > e) return -1;
+    int solve(vector<int>& v, int s, int e) {
+        if (s > e) return s;
 
-//         int mid = s +(e - s)/2;
-
-//         if(v[mid] == mid){
-//             return solve(v , mid+1, e);
-//             return solve(v , mid-1, e);
-//         }else{
-//             return mid-1;
-//         }
-//     }
+        int mid = s +(e - s)/2;
+        
+        if(v[mid] == mid) return solve(v , mid+1 , e);
+        else return solve(v ,s , mid-1);
+    }
 
 public:
     int missingNumber(vector<int>& nums) {
-        // return solve(nums , 0 , nums.size());
-        
-        int a=nums.size();
-
-        for(int i =0 ; i < nums.size(); i++) {
-            a+=i-nums[i];
-        }
-
-        return a;
+        sort(nums.begin() , nums.end()); 
+        return solve(nums , 0 , nums.size()-1);
 
     }
 };
