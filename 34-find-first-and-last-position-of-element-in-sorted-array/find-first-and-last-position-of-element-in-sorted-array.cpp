10a4ecf8e27312f05d1961767761fa3498e71260
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-27 19:04:08 +0530

    Time: 7 ms (84.46%) | Memory: 13.5 MB (69.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,21 @@
 class Solution {
+private:
+
+    int lower_b(vector<int>& v , int s , int e , int k) {
+
+        if(s>e) return s;
+        int mid=s+(e-s)/2;
+
+        if(v[mid] < k) return lower_b(v, mid+1, e , k );
+        else return lower_b(v, s ,mid -1 ,k);
+
+    }
+
+
 public:
     vector<int> searchRange(vector<int>& v, int k) {
-
-        int s=0 , e =v.size()-1;
-        while(s <= e) {
-            int mid = s+ (e-s)/2;
-
-            if(v[mid]<k) s=mid+1;
-            else e=mid-1;
-        }
-        int idx1= s;
-        s=0 , e =v.size()-1;
-        while(s <= e) {
-            int mid = s + (e -s)/2;
-
-            if(v[mid]<(k+1)) s=mid+1;
-            else e=mid-1;
-        }
-        int idx2= s-1;
+        int idx1 =lower_b(v, 0 , v.size()-1 ,k );
+        int idx2= lower_b(v, 0 , v.size()-1 ,k+1 )-1;
         if(idx1 < v.size() && v[idx1] == k) return {idx1,idx2};
         else return {-1,-1};
 
