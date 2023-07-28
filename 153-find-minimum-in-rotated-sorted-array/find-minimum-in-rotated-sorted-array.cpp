bcda69f5bea4c28406f2cf253780b8187b6af0d0
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2023-07-28 19:08:16 +0530

    Time: 3 ms (78.71%) | Memory: 10 MB (91.86%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,6 +1,15 @@
 class Solution {
-private:
+// private:
 
+//     int bin(vector<int>& v , int s , int e) {
+//         if(s>=e) return v[s];
+
+//         int mid =s + (e-s)/2;
+//         if(v[mid] <= v[e] ) return bin(v , s ,mid);
+//         else return bin(v, mid+1 ,e);
+//     }    
+
+public:
     int bin(vector<int>& v , int s , int e) {
         if(s>=e) return v[s];
 
@@ -8,8 +17,6 @@ private:
         if(v[mid] <= v[e] ) return bin(v , s ,mid);
         else return bin(v, mid+1 ,e);
     }    
-
-public:
 
     int findMin(vector<int>& v) {
         int n=v.size()-1;
