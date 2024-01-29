2217139157503f87fe120307783fa76fad439a99
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2024-01-29 11:13:32 +0530

    Time: 3 ms (47.84%) | Memory: 7.5 MB (26.00%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> v = {1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 } ;
+    vector<vector<int > > w ;
+
+    void rec(int k , int i , int n, vector<int >& s) {
+
+        if(i == v.size() ) {
+
+            if(s.size() == k){
+                int sum = 0 ;
+                for(auto &i: s) {sum+=i;}
+                if(n == sum) {
+                    w.push_back(s) ;
+                }
+            }
+            return ;
+        }
+        s.push_back(v[i]);
+        rec(k, i+1 , n , s) ;
+        s.pop_back() ;
+        rec(k, i+1 , n , s ) ;
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int > s ;
+        rec(k , 0 , n , s ) ;
+        return w ;
+    }
+};
